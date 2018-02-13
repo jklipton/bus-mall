5d5ba163dfb1dd516c502c7626757aecefb986ad
Jen Lipton <jkl.lipton@gmail.com>
2018-02-12 16:37:08 -0800

    Works

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,9 @@
 
 const game = {
     products: [],
+    boxes: document.querySelectorAll('.box'),
+    picks: 0,
+
     start: function () {
         this.products.push(
             new product('R2D2 Bag', 'assets/images/bag.jpg'),
@@ -14,7 +17,44 @@ const game = {
             new product('Modern Chair', 'assets/images/chair.jpg'),
             new product('Cthulhu Figurine', 'assets/images/cthulhu.jpg')
         );
+        
+        this.getRandomProducts();
+        const board = document.getElementById('choices');
+
+        board.addEventListener('click', function () {
+            console.log('game was clicked', event.target.title);
+            const choice = event.target.title;
+
+            for (let i = 0; i < game.products.length; i++){
+                if (game.products[i].name === choice){
+                    game.products[i].selections++;
+                    game.picks++;
+                    game.next();
+                }
+            }
+        });
     },
+
+    next: function (){
+        this.clearBoard();
+
+        if (game.picks <= 25){
+            game.getRandomProducts();
+        } else {
+            const table = document.getElementById('table');
+            const h1 = document.createElement('h1');
+            h1.textContent = 'Results:'
+            table.appendChild(h1);
+
+            for (let i = 0; game.products.length; i++){
+                const pLine = document.createElement('p');
+                pLine.textContent = game.products[i].name + ' was clicked ' + game.products[i].selections + ' times!';
+                table.appendChild(pLine);
+            }
+        }
+
+    },
+
     getRandomProducts: function () {
         const shownProducts = [];
 
@@ -24,24 +64,27 @@ const game = {
 
             if (shownProducts.indexOf(option) === -1){
                 shownProducts.push(option);
+                this.products[random].displays++;
             };
         };
-        
-        const boxOne = document.getElementById('one');
-        const boxTwo = document.getElementById('two');
-        const boxThree = document.getElementById('three');
-        const boxes = [boxOne, boxTwo, boxThree];
 
         for (let i = 0; i < 3; i++){
-            const img = document.createElement('img');
-            img.src = shownProducts[i].imageUrl;
-            img.setAttribute('alt', this.name);
+            const img = document.createElement('div');
+            img.setAttribute('title', shownProducts[i].name);
+            img.setAttribute('style', `background-image: url(${shownProducts[i].imageUrl})`);
+            img.setAttribute('class', 'click');
 
-            boxes[i].appendChild(img);
+            this.boxes[i].appendChild(img);
         }
+        console.table(shownProducts);
+
     },
 
-
+    clearBoard: function (){
+        for (let i = 0; i < game.boxes.length; i++){
+            game.boxes[i].textContent = '';    
+        }
+    },
 }
 
 
