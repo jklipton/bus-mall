592fc55ed571a607301953b95c7788d5bf9a4d35
Jen Lipton <jkl.lipton@gmail.com>
2018-02-13 10:11:18 -0800

    Code review changes

--- assets/app.js
+++ assets/app.js
@@ -5,66 +5,37 @@
 const game = {
     products: [],
     boxes: document.querySelectorAll('.box'),
+    board: document.getElementById('board'),
     picks: 0,
 
-    start: function () {
+    load: function () {
         this.products.push(
-            new product('R2D2 Bag', 'assets/images/bag.jpg'),
-            new product('Banana Slicer', 'assets/images/banana.jpg'),
-            new product('TP Tablet Stand', 'assets/images/bathroom.jpg'),
-            new product('Sandal Boots', 'assets/images/boots.jpg'),
-            new product('Breakfast Station', 'assets/images/breakfast.jpg'),
-            new product('Modern Chair', 'assets/images/chair.jpg'),
-            new product('Cthulhu Figurine', 'assets/images/cthulhu.jpg'),
-            new product('Ducky Muzzle', 'assets/images/dog-duck.jpg'),
-            new product('Dragon Meat', 'assets/images/dragon.jpg'),
-            new product('Pen Silverware', 'assets/images/pen.jpg'),
-            new product('Pet Sweeps', 'assets/images/pet-sweep.jpg'),
-            new product('Pizza Scissors', 'assets/images/scissors.jpg'),
-            new product('Shark Sleeping Bag', 'assets/images/shark.jpg'),
-            new product('Baby Sweeps', 'assets/images/sweep.png'),
-            new product('Tauntaun Sleeping Bag', 'assets/images/tauntaun.jpg'),
-            new product('Unicorn Meat', 'assets/images/unicorn.jpg'),
-            new product('USB Tentacle', 'assets/images/usb.gif'),
-            new product('Artsy Watering Can', 'assets/images/water-can.jpg'),
-            new product('Modern Wine Glass', 'assets/images/wine-glass.jpg'),
+            new Product('R2D2 Bag', 'assets/images/bag.jpg'),
+            new Product('Banana Slicer', 'assets/images/banana.jpg'),
+            new Product('TP Tablet Stand', 'assets/images/bathroom.jpg'),
+            new Product('Sandal Boots', 'assets/images/boots.jpg'),
+            new Product('Breakfast Station', 'assets/images/breakfast.jpg'),
+            new Product('Modern Chair', 'assets/images/chair.jpg'),
+            new Product('Cthulhu Figurine', 'assets/images/cthulhu.jpg'),
+            new Product('Ducky Muzzle', 'assets/images/dog-duck.jpg'),
+            new Product('Dragon Meat', 'assets/images/dragon.jpg'),
+            new Product('Pen Silverware', 'assets/images/pen.jpg'),
+            new Product('Pet Sweeps', 'assets/images/pet-sweep.jpg'),
+            new Product('Pizza Scissors', 'assets/images/scissors.jpg'),
+            new Product('Shark Sleeping Bag', 'assets/images/shark.jpg'),
+            new Product('Baby Sweeps', 'assets/images/sweep.png'),
+            new Product('Tauntaun Sleeping Bag', 'assets/images/tauntaun.jpg'),
+            new Product('Unicorn Meat', 'assets/images/unicorn.jpg'),
+            new Product('USB Tentacle', 'assets/images/usb.gif'),
+            new Product('Artsy Watering Can', 'assets/images/water-can.jpg'),
+            new Product('Modern Wine Glass', 'assets/images/wine-glass.jpg'),
         );
-        
-        this.getRandomProducts();
-        const board = document.getElementById('choices');
-
-        board.addEventListener('click', function () {
-            console.log('game was clicked', event.target.title);
-            const choice = event.target.title;
-
-            for (let i = 0; i < game.products.length; i++){
-                if (game.products[i].name === choice){
-                    game.products[i].selections++;
-                    game.picks++;
-                    game.next();
-                }
-            }
-        });
     },
 
-    next: function (){
-        this.clearBoard();
-
-        if (game.picks <= 25){
-            game.getRandomProducts();
-        } else {
-            const table = document.getElementById('table');
-            const h1 = document.createElement('h1');
-            h1.textContent = 'Results:'
-            table.appendChild(h1);
-
-            for (let i = 0; i < game.products.length; i++){
-                const pLine = document.createElement('p');
-                pLine.textContent = game.products[i].name + ' was clicked ' + game.products[i].selections + ' times!';
-                table.appendChild(pLine);
-            }
-        }
-
+    start: function () {
+        this.load();
+        this.getRandomProducts();
+        this.board.addEventListener('click', this.clickHandler);
     },
 
     getRandomProducts: function () {
@@ -92,9 +63,43 @@ const game = {
 
     },
 
+    continue: function (){
+        this.clearBoard();
+
+        if (this.picks <= 25){
+            this.getRandomProducts();
+        } else {
+            this.board.removeEventListener('click', this.clickHandler);
+
+            const table = document.getElementById('table');
+            const h1 = document.createElement('h1');
+            h1.textContent = 'Results:'
+            table.appendChild(h1);
+
+            for (let i = 0; i < this.products.length; i++){
+                const pLine = document.createElement('p');
+                pLine.textContent = this.products[i].name + ' was clicked ' + this.products[i].selections + ' times!';
+                table.appendChild(pLine);
+            }
+        }
+    },
+
+    clickHandler (){
+            const choice = event.target.title;
+
+            if (choice === '') return;
+            for (let i = 0; i < game.products.length; i++){
+                if (game.products[i].name === choice){
+                    game.products[i].selections++;
+                    game.picks++;
+                    game.continue();
+                }
+            }
+    },
+
     clearBoard: function (){
-        for (let i = 0; i < game.boxes.length; i++){
-            game.boxes[i].textContent = '';    
+        for (let i = 0; i < this.boxes.length; i++){
+            this.boxes[i].textContent = '';    
         }
     },
 }
@@ -102,7 +107,7 @@ const game = {
 game.start();
 
 // Object
-function product (name, imageUrl){
+function Product (name, imageUrl){
     this.name= name;
     this.imageUrl = imageUrl;
 
