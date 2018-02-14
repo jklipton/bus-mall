59f37c822200315e93932dfcfff814867532740e
Jen Lipton <jkl.lipton@gmail.com>
2018-02-14 15:56:58 -0800

    second chart

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const startGame= document.getElementById('startGame')
+startGame.addEventListener('click', function(){
+    const intro = document.getElementById('intro');
+    intro.setAttribute('style', 'display: none;');
+
+    game.start();
+});
+
 const game = {
     products: [],
     boxes: document.querySelectorAll('.box'),
@@ -7,38 +15,40 @@ const game = {
     picks: 0,
 
     load: function () {
-        const startGame= document.getElementById('startGame')
-        startGame.addEventListener('click', function(){
-            const intro = document.getElementById('intro');
-            intro.setAttribute('style', 'display: none;');
-    
-            game.start();
-        });
+        // if (localStorage.getItem('products')){
+        //     const prodArray = JSON.parse(localStorage.getItem('products'));
+
+        //     for (let i = 0; i < prodArray.length; i++){
+        //         const product = new Product(prodArray[i].name, prodArray[i].imageUrl, prodArray[i].displays, prodArray[i].selections);
+        //         this.products.push(product);
+        //         }            
+        // } else {
+            this.products.push(
+                new Product('R2D2 Bag', 'assets/images/bag.jpg', 0, 0),
+                new Product('Banana Slicer', 'assets/images/banana.jpg', 0, 0),
+                new Product('TP Tablet Stand', 'assets/images/bathroom.jpg', 0, 0),
+                new Product('Sandal Boots', 'assets/images/boots.jpg', 0, 0),
+                new Product('Meatball Bubblegum', 'assets/images/bubblegum.jpg', 0, 0),
+                new Product('Breakfast Station', 'assets/images/breakfast.jpg', 0, 0),
+                new Product('Modern Chair', 'assets/images/chair.jpg', 0, 0),
+                new Product('Cthulhu Figurine', 'assets/images/cthulhu.jpg', 0, 0),
+                new Product('Ducky Muzzle', 'assets/images/dog-duck.jpg', 0, 0),
+                new Product('Dragon Meat', 'assets/images/dragon.jpg', 0, 0),
+                new Product('Pen Silverware', 'assets/images/pen.jpg', 0, 0),
+                new Product('Pet Sweeps', 'assets/images/pet-sweep.jpg', 0, 0),
+                new Product('Pizza Scissors', 'assets/images/scissors.jpg', 0, 0),
+                new Product('Shark Sleeping Bag', 'assets/images/shark.jpg', 0, 0),
+                new Product('Baby Sweeps', 'assets/images/sweep.png', 0, 0),
+                new Product('Tauntaun Sleeping Bag', 'assets/images/tauntaun.jpg', 0, 0),
+                new Product('Unicorn Meat', 'assets/images/unicorn.jpg', 0, 0),
+                new Product('USB Tentacle', 'assets/images/usb.gif', 0, 0),
+                new Product('Artsy Watering Can', 'assets/images/water-can.jpg', 0, 0),
+                new Product('Modern Wine Glass', 'assets/images/wine-glass.jpg', 0, 0),
+            );
+        // }
     },
 
     start: function () {
-        this.products.push(
-            new Product('R2D2 Bag', 'assets/images/bag.jpg'),
-            new Product('Banana Slicer', 'assets/images/banana.jpg'),
-            new Product('TP Tablet Stand', 'assets/images/bathroom.jpg'),
-            new Product('Sandal Boots', 'assets/images/boots.jpg'),
-            new Product('Meatball Bubblegum', 'assets/images/bubblegum.jpg'),
-            new Product('Breakfast Station', 'assets/images/breakfast.jpg'),
-            new Product('Modern Chair', 'assets/images/chair.jpg'),
-            new Product('Cthulhu Figurine', 'assets/images/cthulhu.jpg'),
-            new Product('Ducky Muzzle', 'assets/images/dog-duck.jpg'),
-            new Product('Dragon Meat', 'assets/images/dragon.jpg'),
-            new Product('Pen Silverware', 'assets/images/pen.jpg'),
-            new Product('Pet Sweeps', 'assets/images/pet-sweep.jpg'),
-            new Product('Pizza Scissors', 'assets/images/scissors.jpg'),
-            new Product('Shark Sleeping Bag', 'assets/images/shark.jpg'),
-            new Product('Baby Sweeps', 'assets/images/sweep.png'),
-            new Product('Tauntaun Sleeping Bag', 'assets/images/tauntaun.jpg'),
-            new Product('Unicorn Meat', 'assets/images/unicorn.jpg'),
-            new Product('USB Tentacle', 'assets/images/usb.gif'),
-            new Product('Artsy Watering Can', 'assets/images/water-can.jpg'),
-            new Product('Modern Wine Glass', 'assets/images/wine-glass.jpg'),
-        );
         this.getRandomProducts();
         this.board.addEventListener('click', this.clickHandler);
     },
@@ -81,43 +91,97 @@ const game = {
 
     drawResults: function () {
         this.board.setAttribute('style', 'border: none;');
-        document.getElementById('results').setAttribute('style', 'display: block;');
-        //data
+        document.getElementById('results').setAttribute('style', 'display: flex;');
+
+        //user data
         const names = [];
         const selections = [];
         const displays = [];
         for(let i = 0; i < this.products.length; i++){
             names.push(this.products[i].name);
             selections.push(this.products[i].selections);
-            displays.push(this.products[i].displays);
         };
 
-        //chart
-        const canvas = document.getElementById('chart');
+        //user results
+        const canvas = document.getElementById('pieChart');
         const ctx = canvas.getContext('2d');
 
-        const barChart = new Chart(ctx, {
+        const userPie = new Chart(ctx,{
+            type: 'doughnut',
+            maintainAspectRatio: false,
+            data: {
+                labels: names,
+                datasets: [{
+                    label: 'times chosen',
+                    data: selections,
+                    backgroundColor: [
+                        '#E07563','#E17277','#DC738C','#D1789F','#BE7FAF',
+                        '#A788BB','#8A90C2','#6A97C2','#499DBC','#26A2B0',
+                        '#0FA4A0','#21A58C','#3DA577','#57A362','#70A04F',
+                        '#879C40','#9D9637','#B28E36','#C4863C','#D27E49',
+                    ]
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Your Picks:',
+                },
+                legend: {
+                    display: false,
+                },
+            },
+        });
+
+        // persistant results
+        let storedData = []
+        if (localStorage.getItem('products')){
+            storedData = JSON.parse(localStorage.getItem('products'));
+
+            for (let i = 0; i < storedData.length; i++){
+                const userSelections = this.products[i].selections;
+                const userDisplays = this.products[i].displays;
+
+                storedData[i].selections += userSelections;
+                storedData[i].displays += userDisplays;
+                }
+        }
+        localStorage.setItem('products', JSON.stringify(storedData));
+
+        const totalSelects = [];
+        const totalDisplays = [];
+
+        for(let i = 0; i < storedData.length; i++){
+            totalSelects.push(storedData[i].selections);
+            totalDisplays.push(storedData[i].displays);
+        };
+
+        const canvas2 = document.getElementById('barChart');
+        const ctx2 = canvas2.getContext('2d');
+
+        const barChart = new Chart(ctx2, {
             type: 'horizontalBar',
             data: {
                 labels: names,
                 datasets: [{
-                    label: 'number of times displayed',
-                    data: displays,
+                    label: 'total times displayed',
+                    data: totalDisplays,
                     backgroundColor: '#8C8C8C',
                     stack: 'Together',
                 },
                 {
-                    label: 'number of times selected',
-                    data: selections,
+                    label: 'total times selected',
+                    data: totalSelects,
                     backgroundColor: '#ADCDFF',
                     stack: 'Together',
 
                 }]
             },
             options: {
+                maintainAspectRatio: false,
                 title: {
                     display: true,
-                    text: 'Results',
+                    text: 'Total Results',
                 },
                 legend: {
                     display: true,
@@ -126,15 +190,11 @@ const game = {
                 scales: {
                     yAxes: [{
                         stacked: true,
-                        ticks: {
-                            autoSkip: false,
-                        }
                     }],
                     xAxes: [{
                         stacked: true,
                         ticks: {
                             beginAtZero: true,
-                            stepSize: 1,
                         },
                     }]
                 },
@@ -166,10 +226,10 @@ game.load();
 
 
 // Object
-function Product (name, imageUrl){
+function Product (name, imageUrl, displays, selections){
     this.name= name;
     this.imageUrl = imageUrl;
 
-    this.displays = 0;
-    this.selections = 0;
+    this.displays = displays;
+    this.selections = selections;
 }
